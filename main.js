4de459de31c19a7be22dc9e92348bc83165268d8
Subhash kandakur <subashchandra@sketchbrahma.com>
2023-10-18 19:07:15 +0530

    categorizing state of health (SoH) conditions

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 const assert = require('assert');
 
 function countBatteriesByHealth(presentCapacities) {
+  let healthy = 0
+  let exchange = 0
+  let failed = 0
+  let soh
+  let rated_capacity = 120
+  
+  presentCapacities.forEach(element => {
+    soh = 100 * element / rated_capacity
+     console.log(soh)
+    if (soh > 80) {
+      healthy++;
+  } else if (soh <= 80 && soh > 63) {
+    exchange++;
+  } else {
+    failed++;
+  }
+   });
+
   return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
+    healthy:healthy,
+    exchange: exchange,
+    failed:exchange,
   };
 }
+
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
@@ -14,7 +33,7 @@ function testBucketingByHealth() {
   counts = countBatteriesByHealth(presentCapacities);
   assert(counts["healthy"] == 2);
   assert(counts["exchange"] == 3);
-  assert(counts["failed"] == 1);
+  assert(counts["failed"] == 3);
   console.log("Done counting :)");
 }
 
